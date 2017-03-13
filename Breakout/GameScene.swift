d2fe855b2c8127bf398bc6885920e5467df39a41
MRisser1 <student@MRisser1.local>
2017-03-13 11:46:06 -0500

    Create paddle object.

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -12,10 +12,12 @@ import GameplayKit
 class GameScene: SKScene {
 
     var ball = SKShapeNode()
+    var paddle = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         createBackground()
         makeBall()
+        makePaddle()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -61,6 +63,15 @@ class GameScene: SKScene {
         ball.physicsBody?.linearDamping = 0
         ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
         addChild(ball)
+    }
+    
+    func makePaddle() {
+        paddle = SKSpriteNode(color: UIColor.white, size: CGSize(width: frame.width/4, height: frame.height/25))
+        paddle.position = CGPoint(x: frame.midX, y: frame.minY+125)
+        paddle.name = "paddle"
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        paddle.physicsBody?.isDynamic = false
+        addChild(paddle)
     }
 
 }
