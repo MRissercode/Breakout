25edcbebdf62ff39ad44bedbdecb5a03e31efcef
MRisser1 <student@MRisser1.local>
2017-03-14 11:27:23 -0500

    Created ball dynamic behavior.

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -9,17 +9,22 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         createBackground()
         makeBall()
         makePaddle()
         makeBrick()
+        makeLoseZone()
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 3))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -77,12 +82,20 @@ class GameScene: SKScene {
     }
     
     func makeBrick() {
-        brick = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width/5, height: 50))
+        brick = SKSpriteNode(color: UIColor.blue, size: CGSize(width: frame.width/5, height: 50))
         brick.position = CGPoint(x: frame.midX, y: frame.maxY-30)
         brick.name = "brick"
         brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
         brick.physicsBody?.isDynamic = false
         addChild(brick)
+    }
+    
+    func makeLoseZone() {
+        let loseZone = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width, height: 50))
+        loseZone.position = CGPoint(x: frame.midX, y: frame.minY+25)
+        loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
+        loseZone.physicsBody?.isDynamic = false
+        addChild(loseZone)
     }
 
 }
