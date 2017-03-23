e6a27075a859f2ac15b99701b269c1ce2d5991b5
MRisser1 <student@MRisser1.local>
2017-03-23 11:43:27 -0500

    Trying merge with partner.

--- Breakout/GameViewController.swift
+++ Breakout/GameViewController.swift
@@ -5,20 +5,21 @@
 //  Created by MRisser1 on 3/13/17.
 //  Copyright © 2017 MRisser1. All rights reserved.
 //
+
 import UIKit
 import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
     
-        override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFit
                 
                 // Present the scene
                 view.presentScene(scene)
@@ -29,12 +30,7 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
-       
-
     }
-    
-    
-
     
     override var shouldAutorotate: Bool {
         return true
